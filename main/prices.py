3725b5631059528d22ac71871cef658b1d1aee11
Marcus Schimizzi <marcjschimizzi@gmail.com>
2017-05-19 11:35:24 -0400

    added get today

--- main/prices.py
+++ main/prices.py
@@ -6,6 +6,8 @@ import sys
 import datetime
 
 REGEX = '<span data-reactid="62">.*<\/span>'
+REGEX2 = 'id="Lead-2-QuoteHeader-Proxy".*'
+REGEX3 = 'data-reactid="36".*'
 
 def fetch(url):
 	r = requests.get(url)
@@ -25,9 +27,18 @@ if day_of_week is 5:
 elif day_of_week is 6:
 	day_begin -= (2 * 60 * 60 *24) # convert Sunday to Friday
 data = fetch("https://finance.yahoo.com/quote/" + symbol + "/history?period1=" + str(day_begin) + "&period2=" + str(day_begin) + "&interval=1d&filter=history&frequency=1d")
-data = re.search(REGEX, data)
-if data:
-	data = data.group(0)
-	data = re.sub("</span>.*", "", data)
+cleaned_data = re.search(REGEX, data)
+if cleaned_data:
+	cleaned_data = cleaned_data.group(0)
+	cleaned_data = re.sub("</span>.*", "", cleaned_data)
 	closing = data.split('>')[1]
+	print closing
+else:
+	# get current price if day entered is today (no closing yet)
+	cleaned_data = re.search(REGEX2, data)
+	cleaned_data = cleaned_data.group(0)
+	cleaned_data = re.search(REGEX3, cleaned_data)
+	cleaned_data = cleaned_data.group(0)
+	cleaned_data = re.sub("</span>.*", "", cleaned_data)
+	closing = cleaned_data.split('>')[1]
 	print closing
