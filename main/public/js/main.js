4879d7929d2169b3e982f3a0d87467e7d049c759
Daniel Stratton <dstrat28@gmail.com>
2017-05-19 10:15:21 -0400

    Updates

--- main/public/js/main.js
+++ main/public/js/main.js
@@ -4,10 +4,21 @@ data.controller("stock", function($scope, $http) {
     var url = window.location.href.split('/');
     var stock = url.pop();
     $http.get('/stock/' + stock).success(function(data, status, headers, config) {
-        $('#loading').hide();
-        $scope.data = data;
-        $('#loaded').show();
-        console.log(data);
+        if (data.company) {
+            $('#loading').hide();
+            $scope.data = data;
+            for (var i = 0; i < data.all.length; i++) {
+                data.all[i].created = data.all[i].created * 1000;
+            }
+            $('#loaded').show();
+            $('#loaded2').show();
+            console.log(data);
+        } else {
+            $('#loading').hide();
+            $scope.ticker = stock.toUpperCase();
+            $('#invalid').show();
+        }
+
     });
 
 });
