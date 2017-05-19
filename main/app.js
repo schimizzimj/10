450c1280f1f2e16e819580a665a78076a313bbcd
Daniel Stratton <dstrat28@gmail.com>
2017-05-19 11:49:34 -0400

    Updates

--- main/app.js
+++ main/app.js
@@ -45,110 +45,120 @@ app.get('/stock/*', function(req, res) {
             res.send({
                 "company": "Tesla, Inc.",
                 "ticker": "TSLA",
-                "sentiment": "Neutral (0)",
-                "positive": 0,
-                "negative": 0,
+                "sentiment": "Slightly Positive (0.20)",
+                "positive": 6,
+                "negative": 4,
                 "all": [
                     {
-                        "url": "http://www.bing.com/news/apiclick.aspx?ref=FexRss&aid=&tid=D254857CE7774D7EB6868C764486E532&url=http%3a%2f%2finvestorplace.com%2f2017%2f05%2fthe-tesla-inc-tsla-stock-factor-everyone-ignores-the-economy%2f&c=10657904083778782780&mkt=en-us",
+                        "url": "http://www.bing.com/news/apiclick.aspx?ref=FexRss&aid=&tid=3356248C025B401C90A3CC83F5F7B2E8&url=http%3a%2f%2finvestorplace.com%2f2017%2f05%2fthe-tesla-inc-tsla-stock-factor-everyone-ignores-the-economy%2f&c=10657904083778782780&mkt=en-us",
                         "title": "The Tesla Inc (TSLA) Stock Factor Everyone Ignores: The Economy",
                         "description": "With Tesla shares up more than 45% in 2017, those bears are clearly holding on tight through a lot of pain. But how long can TSLA keep defying gravity? While people typically break down Tesla by running up and down every one of its latest updates ...",
                         "created": 1495175520,
-                        "price": "False",
-                        "pos": 0,
-                        "neg": 0,
-                        "total": 0,
-                        "tag": "Invalid"
+                        "price": "313.76",
+                        "pos": 15,
+                        "neg": 29,
+                        "total": -14,
+                        "tag": "Negative",
+                        "indNum": "-0.32"
                     }, {
-                        "url": "http://www.bing.com/news/apiclick.aspx?ref=FexRss&aid=&tid=D254857CE7774D7EB6868C764486E532&url=http%3a%2f%2f247wallst.com%2fautos%2f2017%2f05%2f19%2ftesla-in-the-crosshairs-again-over-factory-conditions%2f&c=12428590294388544084&mkt=en-us",
-                        "title": "Tesla in the Crosshairs Again Over Factory Conditions",
-                        "description": "A recent story in The Guardian once more focuses attention on working conditions for the employees who build cars for Tesla Inc. (NASDAQ: TSLA). The report details the toll of the company’s drive to raise production and turn a profit. Tesla has plans to ...",
-                        "created": 1495175940,
-                        "price": "False",
-                        "pos": 0,
-                        "neg": 0,
-                        "total": 0,
-                        "tag": "Invalid"
-                    }, {
-                        "url": "http://www.bing.com/news/apiclick.aspx?ref=FexRss&aid=&tid=D254857CE7774D7EB6868C764486E532&url=https%3a%2f%2fwww.smarteranalyst.com%2f2017%2f05%2f18%2fwarren-buffet-buy-tesla-inc-tsla%2f&c=17150490631485472268&mkt=en-us",
+                        "url": "http://www.bing.com/news/apiclick.aspx?ref=FexRss&aid=&tid=3356248C025B401C90A3CC83F5F7B2E8&url=https%3a%2f%2fwww.smarteranalyst.com%2f2017%2f05%2f18%2fwarren-buffet-buy-tesla-inc-tsla%2f&c=17150490631485472268&mkt=en-us",
                         "title": "Should Warren Buffet Buy Tesla Inc (TSLA)?",
                         "description": "During Berkshire’s annual meeting, Warren Buffet has acknowledged regret by having missed on the Google and Amazon. All his talk about staying within the circle of competence has suddenly vanished. In my opinion, Buffet didn’t miss it, he just remained ...",
                         "created": 1495116000,
-                        "price": "False",
-                        "pos": 88,
+                        "price": 0,
+                        "pos": 73,
                         "neg": 58,
-                        "total": 30,
-                        "tag": "Positive"
+                        "total": 15,
+                        "tag": "Positive",
+                        "indNum": "0.11"
                     }, {
-                        "url": "http://www.bing.com/news/apiclick.aspx?ref=FexRss&aid=&tid=D254857CE7774D7EB6868C764486E532&url=http%3a%2f%2fbuffalonews.com%2f2017%2f05%2f16%2fceo-lyndon-rive-leave-tesla-june%2f&c=3150502662314864408&mkt=en-us",
-                        "title": "SolarCity co-founder Lyndon Rive to leave Tesla Inc. in June",
-                        "description": "Lyndon Rive, who led SolarCity as it developed plans to open a massive solar factory in South Buffalo, will leave the Tesla Inc. subsidiary next month. Rive told Reuters on Monday he is leaving to pursue a new startup sometime next year. Rive co ...",
-                        "created": 1494896400,
-                        "price": "False",
-                        "pos": 0,
-                        "neg": 0,
-                        "total": 0,
-                        "tag": "Invalid"
-                    }, {
-                        "url": "http://www.bing.com/news/apiclick.aspx?ref=FexRss&aid=&tid=D254857CE7774D7EB6868C764486E532&url=http%3a%2f%2fwww.gamengadgets.com%2ftesla-tops-u-s-auto-industry-electric-innovations-plays-primary-growth-driver%2f&c=14377692146644011035&mkt=en-us",
+                        "url": "http://www.bing.com/news/apiclick.aspx?ref=FexRss&aid=&tid=3356248C025B401C90A3CC83F5F7B2E8&url=http%3a%2f%2fwww.gamengadgets.com%2ftesla-tops-u-s-auto-industry-electric-innovations-plays-primary-growth-driver%2f&c=14377692146644011035&mkt=en-us",
                         "title": "Tesla Tops The U.S. Auto Industry, Electric Innovations Plays The Primary Growth Driver",
                         "description": "Not even a couple of years back, people weren’t even sure of the future of Tesla Inc., when Elon Musk, the CEO of the company, had set a very unrealistic timeline for the Model 3. This luxury sedan is a result of all the aggressive instincts on their part.",
                         "created": 1495162740,
-                        "price": "False",
-                        "pos": 59,
+                        "price": "313.8599",
+                        "pos": 44,
                         "neg": 35,
-                        "total": 24,
-                        "tag": "Positive"
+                        "total": 9,
+                        "tag": "Positive",
+                        "indNum": "0.11"
                     }, {
-                        "url": "http://www.bing.com/news/apiclick.aspx?ref=FexRss&aid=&tid=D254857CE7774D7EB6868C764486E532&url=https%3a%2f%2fmarketexclusive.com%2ftesla-factory-bad-place-workers%2f109751%2f&c=1247742823432785040&mkt=en-us",
+                        "url": "http://www.bing.com/news/apiclick.aspx?ref=FexRss&aid=&tid=3356248C025B401C90A3CC83F5F7B2E8&url=https%3a%2f%2fmarketexclusive.com%2ftesla-factory-bad-place-workers%2f109751%2f&c=1247742823432785040&mkt=en-us",
                         "title": "Is Tesla Inc (NASDAQ:TSLA) Factory Bad Place for Workers?",
                         "description": "Some workers at Tesla Inc (NASDAQ:TSLA)’s car factory in California are unhappy with working conditions, according to a report from The Guardian. The newspaper talked to many people and some have complained about grueling pressure and sometimes life ...",
                         "created": 1495126380,
-                        "price": "False",
-                        "pos": 43,
-                        "neg": 40,
-                        "total": 3,
-                        "tag": "Negative"
+                        "price": 0,
+                        "pos": 28,
+                        "neg": 38,
+                        "total": -10,
+                        "tag": "Negative",
+                        "indNum": "-0.15"
                     }, {
-                        "url": "http://www.bing.com/news/apiclick.aspx?ref=FexRss&aid=&tid=D254857CE7774D7EB6868C764486E532&url=http%3a%2f%2fwww.profitconfidential.com%2fstock%2fopportunity-tesla-stock-fast-approaching%2f&c=15558450282620847363&mkt=en-us",
-                        "title": "An Opportunity in Tesla Stock Is Fast Approaching",
-                        "description": "I am providing a quick update on Tesla Inc (NASDAQ:TSLA) stock because I believe that the current sell-off that is gripping the equity markets is based on political rhetoric surrounding Donald Trump, which will prove to be a temporary speed bump.",
-                        "created": 1495069200,
-                        "price": "False",
-                        "pos": 262,
-                        "neg": 177,
-                        "total": 85,
-                        "tag": "Positive"
+                        "url": "http://www.bing.com/news/apiclick.aspx?ref=FexRss&aid=&tid=3356248C025B401C90A3CC83F5F7B2E8&url=http%3a%2f%2f247wallst.com%2fautos%2f2017%2f05%2f19%2ftesla-in-the-crosshairs-again-over-factory-conditions%2f&c=12428590294388544084&mkt=en-us",
+                        "title": "Tesla in the Crosshairs Again Over Factory Conditions",
+                        "description": "A recent story in The Guardian once more focuses attention on working conditions for the employees who build cars for Tesla Inc. (NASDAQ: TSLA). The report details the toll of the company’s drive to raise production and turn a profit. Tesla has plans to ...",
+                        "created": 1495175940,
+                        "price": "313.70",
+                        "pos": 19,
+                        "neg": 29,
+                        "total": -10,
+                        "tag": "Negative",
+                        "indNum": "-0.21"
                     }, {
-                        "url": "http://www.bing.com/news/apiclick.aspx?ref=FexRss&aid=&tid=D254857CE7774D7EB6868C764486E532&url=https%3a%2f%2fwww.bloomberg.com%2fnews%2farticles%2f2017-05-16%2ftesla-rebuffed-uber-partnership-on-self-driving-cars-in-2016&c=15071234955649672111&mkt=en-us",
+                        "url": "http://www.bing.com/news/apiclick.aspx?ref=FexRss&aid=&tid=3356248C025B401C90A3CC83F5F7B2E8&url=https%3a%2f%2fwww.bloomberg.com%2fnews%2farticles%2f2017-05-16%2ftesla-rebuffed-uber-partnership-on-self-driving-cars-in-2016&c=15071234955649672111&mkt=en-us",
                         "title": "Tesla Rebuffed Uber Partnership on Self-Driving Cars in 2016",
                         "description": "Uber Technologies Inc. Chief Executive Officer Travis Kalanick rang up Tesla Inc. CEO Elon Musk last year to propose a partnership on self-driving cars, according to an upcoming book. “I said, ‘Look man, we should partner,’” Kalanick recalled in ...",
                         "created": 1495082460,
-                        "price": "313.06",
-                        "pos": 142,
-                        "neg": 126,
-                        "total": 16,
-                        "tag": "Neutral"
+                        "price": 0,
+                        "pos": 127,
+                        "neg": 124,
+                        "total": 3,
+                        "tag": "Positive",
+                        "indNum": "0.01"
                     }, {
-                        "url": "http://www.bing.com/news/apiclick.aspx?ref=FexRss&aid=&tid=D254857CE7774D7EB6868C764486E532&url=http%3a%2f%2fwww.insidermonkey.com%2fblog%2ftesla-inc-tsla-why-this-bear-threw-in-the-towel-581609%2f&c=394711963277342659&mkt=en-us",
+                        "url": "http://www.bing.com/news/apiclick.aspx?ref=FexRss&aid=&tid=3356248C025B401C90A3CC83F5F7B2E8&url=http%3a%2f%2fwww.profitconfidential.com%2fstock%2fopportunity-tesla-stock-fast-approaching%2f&c=15558450282620847363&mkt=en-us",
+                        "title": "An Opportunity in Tesla Stock Is Fast Approaching",
+                        "description": "I am providing a quick update on Tesla Inc (NASDAQ:TSLA) stock because I believe that the current sell-off that is gripping the equity markets is based on political rhetoric surrounding Donald Trump, which will prove to be a temporary speed bump.",
+                        "created": 1495069200,
+                        "price": 0,
+                        "pos": 247,
+                        "neg": 177,
+                        "total": 70,
+                        "tag": "Positive",
+                        "indNum": "0.17"
+                    }, {
+                        "url": "http://www.bing.com/news/apiclick.aspx?ref=FexRss&aid=&tid=3356248C025B401C90A3CC83F5F7B2E8&url=http%3a%2f%2fwww.insidermonkey.com%2fblog%2ftesla-inc-tsla-why-this-bear-threw-in-the-towel-581609%2f&c=394711963277342659&mkt=en-us",
                         "title": "Tesla Inc (TSLA): Why This Bear Threw In The Towel",
                         "description": "While its cars are near-universally praised, Tesla Inc (NASDAQ:TSLA) has plenty of detractors when it comes to its viability as a business model and investment. 27% of the stock’s float is being sold short as of April 28, and numerous bears take swipes ...",
                         "created": 1495094340,
-                        "price": "313.06",
-                        "pos": 59,
+                        "price": 0,
+                        "pos": 44,
                         "neg": 88,
-                        "total": -29,
-                        "tag": "Negative"
+                        "total": -44,
+                        "tag": "Negative",
+                        "indNum": "-0.33"
                     }, {
-                        "url": "http://www.bing.com/news/apiclick.aspx?ref=FexRss&aid=&tid=D254857CE7774D7EB6868C764486E532&url=https%3a%2f%2fstockmarketdaily.co%2f11667-2%2f&c=13235027560964973183&mkt=en-us",
+                        "url": "http://www.bing.com/news/apiclick.aspx?ref=FexRss&aid=&tid=3356248C025B401C90A3CC83F5F7B2E8&url=http%3a%2f%2fbuffalonews.com%2f2017%2f05%2f16%2fceo-lyndon-rive-leave-tesla-june%2f&c=3150502662314864408&mkt=en-us",
+                        "title": "SolarCity co-founder Lyndon Rive to leave Tesla Inc. in June",
+                        "description": "Lyndon Rive, who led SolarCity as it developed plans to open a massive solar factory in South Buffalo, will leave the Tesla Inc. subsidiary next month. Rive told Reuters on Monday he is leaving to pursue a new startup sometime next year. Rive co ...",
+                        "created": 1494896400,
+                        "price": 0,
+                        "pos": 18,
+                        "neg": 14,
+                        "total": 4,
+                        "tag": "Positive",
+                        "indNum": "0.13"
+                    }, {
+                        "url": "http://www.bing.com/news/apiclick.aspx?ref=FexRss&aid=&tid=3356248C025B401C90A3CC83F5F7B2E8&url=https%3a%2f%2fstockmarketdaily.co%2f11667-2%2f&c=13235027560964973183&mkt=en-us",
                         "title": "Why Tesla Inc (NASDAQ:TSLA) Employees Are More Concerned On Safety?",
                         "description": "Tesla Inc (NASDAQ:TSLA) appears to be keen on accomplish its production targets, which are aggressive. Though its CEO, Elon Musk, indicated that he accorded top priority to the worker safety and satisfaction, recent events are worrying factors for its ...",
                         "created": 1495156980,
-                        "price": "False",
-                        "pos": 86,
-                        "neg": 74,
-                        "total": 12,
-                        "tag": "Negative"
+                        "price": "313.86",
+                        "pos": 71,
+                        "neg": 66,
+                        "total": 5,
+                        "tag": "Positive",
+                        "indNum": "0.04"
                     }
                 ]
             });
@@ -159,9 +169,9 @@ app.get('/stock/*', function(req, res) {
                 var negative = 0;
                 var sentiment = "";
                 for (var i = 0; i < scores.length; i++) {
-                    if (scores[i].tag == 'positive') {
+                    if (scores[i].tag == 'Positive') {
                         positive++;
-                    } else if (scores[i].tag == 'negative') {
+                    } else if (scores[i].tag == 'Negative') {
                         negative++;
                     }
                 }
@@ -175,9 +185,9 @@ app.get('/stock/*', function(req, res) {
                     }
                 } else if (num < 0) {
                     if (num < -0.6) {
-                        entiment = "Very Negative (" + num + ")";
+                        sentiment = "Very Negative (" + num + ")";
                     } else {
-                        entiment = "Slightly Negative (" + num + ")";
+                        sentiment = "Slightly Negative (" + num + ")";
                     }
                 } else {
                     sentiment = "Neutral (0)";
@@ -202,7 +212,11 @@ function getPrice(ticker, date, callback) {
         args: [ticker, date]
     });
     pyshell.on('message', function(message) {
-        callback(message);
+        if (isNaN(message)) {
+            callback(0);
+        } else {
+            callback(message);
+        }
     });
 }
 
@@ -257,7 +271,8 @@ function checkUrl(link, callback) {
                 'pos': 0,
                 'neg': 0,
                 'total': 0,
-                'tag': 'Invalid'
+                'tag': 'Invalid',
+                'indNum': 0
             });
         } else {
             text = body.toLowerCase();
@@ -280,15 +295,17 @@ function checkUrl(link, callback) {
                     }
                 }
             }
+            pos = pos - 15;
             var total = pos - neg;
             var tag;
             var indNum = (pos - neg) / (pos + neg);
+            indNum = indNum.toFixed(2);
             //Must have at least 30 pos/neg matches to be used
             if (pos >= 10 && neg >= 10) {
                 //Adjust for data skewed postivie
-                if (total >= 17) {
+                if (total >= 2) {
                     tag = "Positive";
-                } else if (total < 13) {
+                } else if (total < 2) {
                     tag = "Negative";
                 } else {
                     tag = "Neutral";
@@ -302,7 +319,8 @@ function checkUrl(link, callback) {
                     'pos': pos,
                     'neg': neg,
                     'total': total,
-                    'tag': tag
+                    'tag': tag,
+                    'indNum': indNum
                 });
             } else {
                 callback({
@@ -314,7 +332,8 @@ function checkUrl(link, callback) {
                     'pos': 0,
                     'neg': 0,
                     'total': 0,
-                    'tag': 'Invalid'
+                    'tag': 'Invalid',
+                    'indNum': 0
                 });
             }
         }
--- main/public/js/main.js
+++ main/public/js/main.js
@@ -1,17 +1,55 @@
-var data = angular.module("stock_data", []);
+var data = angular.module("stock_data", ['chart.js']);
 
 data.controller("stock", function($scope, $http) {
     var url = window.location.href.split('/');
     var stock = url.pop();
-    $http.get('/stock/' + stock).success(function(data, status, headers, config) {
+    $http.get('/stock/' + stock).then(function(data, status, headers, config) {
+        data = data.data;
         if (data.company) {
             $('#loading').hide();
             $scope.data = data;
+            var chart_dates = [];
+            var sentiment = [];
+            var price = [];
             for (var i = 0; i < data.all.length; i++) {
                 data.all[i].created = data.all[i].created * 1000;
+                chart_dates.push(new Date(data.all[i].created));
+                sentiment.push(data.all[i].indNum);
+                price.push(data.all[i].price);
             }
-            $('#loaded').show();
-            $('#loaded2').show();
+            $scope.labels = chart_dates;
+            $scope.series = ['Series A', 'Series B'];
+            $scope.cData = [sentiment, price];
+            /*
+            $scope.options = {
+                scales: {
+                    yAxes: [
+                        {
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Estado'
+                            },
+                            ticks: {
+                                min: 1,
+                                max: 3,
+                                stepSize: .1,
+                            }
+                        }
+                    ],
+                    xAxes: [
+                        {
+                            scaleLabel: {
+                                display: true,
+                                labelString: 'Hora'
+                            }
+                        }
+                    ]
+                }
+            };
+            */
+
+            $('.loaded').show();
+
             console.log(data);
         } else {
             $('#loading').hide();
